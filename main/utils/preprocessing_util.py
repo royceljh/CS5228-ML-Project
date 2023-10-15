68d10b1d83b2f47acabe2b235d3895cf0f83ced4
Royce <royceljh@gmail.com>
2023-10-15 11:36:29 +0800

    Add flat-specific preprocessing code

--- main/utils/preprocessing_util.py
+++ main/utils/preprocessing_util.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import category_encoders as ce
 
 def get_min(df, feature):
     print('Min: ' + str(df[feature].min()))
@@ -29,7 +30,6 @@ def get_counts(df, feature):
 
 def clean_replace_numeric(df, feature, search, replace):
     df[feature] = np.where(df[feature].str.contains(search, na=False), replace, df[feature])
-    #df[feature] = pd.to_numeric(df[feature], errors='coerce')
 
 def format_numeric(df, feature):
     df[feature] = pd.to_numeric(df[feature], errors='coerce')
@@ -47,3 +47,8 @@ def plot_scatter(df, feature1, feature2):
     plt.ylabel('Monthly Rent')
     plt.show()
 
+def target_encode(df, feature):
+    target_encoder_smooth = ce.TargetEncoder(cols=[feature], smoothing=10)
+    target_encoder_smooth.fit(df[feature], df['monthly_rent'])
+    df[feature] = target_encoder_smooth.transform(df[feature])
+    df[feature] = df[feature].round()
